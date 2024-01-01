09d9fd65591e29dc8119ec2e209fb4eab0a323e9
Ilse Gaertner <joachimpruegl@gmail.com>
2024-01-01 22:49:34 +0100

    Update work

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,32 @@ function fadeInProjects() {
     }, 10 * index); // Adjust the delay as needed
   });
 }
-
 window.addEventListener("load", fadeInProjects);
+
+// // Function to handle the fading effect for elements in viewport
+// function handleFadeIn() {
+//   const elements = document.querySelectorAll(".project-list__item");
+
+//   elements.forEach((element) => {
+//     const positionFromTop = element.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight;
+
+//     // Calculate the threshold for element visibility
+//     const fadeInThreshold = windowHeight * 0.9; // Adjust this value as needed
+
+//     if (positionFromTop < fadeInThreshold && positionFromTop > -windowHeight) {
+//       const opacity = 1 - (fadeInThreshold - positionFromTop) / fadeInThreshold;
+//       element.style.opacity = opacity > 0 ? opacity : 0;
+//     } else if (positionFromTop >= windowHeight) {
+//       element.style.opacity = 0;
+//     } else {
+//       element.style.opacity = 1;
+//     }
+//   });
+// }
+
+// // Event listener for scrolling
+// window.addEventListener("scroll", handleFadeIn);
+
+// // Trigger handleFadeIn initially to check for elements already in the viewport
+// handleFadeIn();
