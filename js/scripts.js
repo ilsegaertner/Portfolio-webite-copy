82a82a93fec9a7abff21c5b31923268475b6ca44
Ilse Gaertner <joachimpruegl@gmail.com>
2024-02-13 16:47:59 -0600

    Add styles and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,10 @@ document.getElementById("contactbutton").addEventListener("click", function () {
   document.querySelector(".project-section").classList.toggle("collapsed");
   document.querySelector(".addressbutton2").classList.toggle("button-expanded");
   document.querySelector(".project-list").classList.toggle("opacity-2");
+  document
+    .getElementById("toggleAsideButton")
+    .classList.toggle("toggleAsideButton-invisible");
+  document.getElementById("aside").classList.toggle("aside-invisible");
 });
 
 document.getElementById("expandButton").addEventListener("click", function () {
@@ -56,4 +60,76 @@ document.getElementById("expandButton").addEventListener("click", function () {
   document.querySelector(".project-section").classList.toggle("collapsed");
   document.querySelector(".addressbutton2").classList.toggle("button-expanded");
   document.querySelector(".project-list").classList.toggle("opacity-2");
+  document
+    .getElementById("toggleAsideButton")
+    .classList.toggle("toggleAsideButton-invisible");
+  document.getElementById("aside").classList.toggle("aside-invisible");
+});
+
+// smooth-scrolling
+
+document.addEventListener("DOMContentLoaded", function () {
+  const workList = document.querySelector(".work-list");
+
+  workList.addEventListener("click", function (event) {
+    if (event.target.tagName === "A") {
+      event.preventDefault();
+      const targetId = event.target.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        scrollToTarget(targetElement, -90);
+      }
+    }
+  });
+
+  function scrollToTarget(targetElement, offset) {
+    const startPosition = window.scrollY;
+    const targetPosition =
+      targetElement.getBoundingClientRect().top + window.scrollY + offset;
+    const distance = targetPosition - startPosition;
+    const duration = 300; // Adjust the duration of the animation
+
+    let startTime = null;
+
+    function animation(currentTime) {
+      if (startTime === null) {
+        startTime = currentTime;
+      }
+
+      const elapsedTime = currentTime - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
+      const easeProgress = ease(progress);
+
+      window.scrollTo(0, startPosition + distance * easeProgress);
+
+      if (elapsedTime < duration) {
+        requestAnimationFrame(animation);
+      }
+    }
+
+    function ease(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    }
+
+    requestAnimationFrame(animation);
+  }
+});
+
+// toggle aside button
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleAsideButton = document.getElementById("toggleAsideButton");
+  const aside = document.getElementById("aside");
+
+  toggleAsideButton.addEventListener("click", function () {
+    console.log("Button clicked!");
+    aside.classList.toggle("open");
+
+    // Toggle button content
+    if (toggleAsideButton.innerHTML === ">") {
+      toggleAsideButton.innerHTML = "<";
+    } else {
+      toggleAsideButton.innerHTML = ">";
+    }
+  });
 });
