92a2b0d57590ff9cb817c62c83d46eb28ae9a52b
Ilse Gaertner <joachimpruegl@gmail.com>
2024-03-04 15:39:54 +0100

    add cards design

--- js/scripts.js
+++ js/scripts.js
@@ -10,34 +10,6 @@ function fadeInProjects() {
 }
 window.addEventListener("load", fadeInProjects);
 
-// expanding the work page with contact-grid
-
-// document.getElementById("contactbutton").addEventListener("click", function () {
-//   var contactGrid = document.querySelector(".contact-grid");
-//   var projectListElement = document.querySelector(".project-list");
-//   var headingClassText = document.querySelector(".headingclass_work");
-
-//   // Toggle the 'expanded' class on the contact grid
-//   contactGrid.classList.toggle("expanded");
-//   projectListElement.classList.toggle("project-list-expanded");
-
-//   // Toggle the text content based on the contact grid's width
-//   if (contactGrid.classList.contains("expanded")) {
-//     headingClassText.textContent = "> contact";
-//   } else {
-//     headingClassText.textContent = "> work";
-//   }
-
-//   // Toggle other classes or perform other actions as needed
-//   document.querySelector(".project-section").classList.toggle("collapsed");
-//   document.querySelector(".addressbutton2").classList.toggle("button-expanded");
-//   document.querySelector(".project-list").classList.toggle("opacity-2");
-//   document
-//     .getElementById("toggleAsideButton")
-//     .classList.toggle("toggleAsideButton-invisible");
-//   document.getElementById("aside").classList.toggle("aside-invisible");
-// });
-
 document.getElementById("expandButton").addEventListener("click", function () {
   var contactGrid = document.querySelector(".contact-grid");
   var projectListElement = document.querySelector(".project-list");
