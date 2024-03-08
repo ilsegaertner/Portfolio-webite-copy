8a93b15804cb7936113a78038ef0a9ce413df310
Ilse Gaertner <joachimpruegl@gmail.com>
2024-03-08 12:30:44 +0100

    implement case study, adjust h1 font sizing

--- js/scripts.js
+++ js/scripts.js
@@ -145,3 +145,21 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+// Swiper Case-Study
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper3 = new Swiper(".mySwiper3", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: ["-20%", 0, -1],
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
+  });
+});
